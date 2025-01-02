14d5b2a9032c4f542ac9f2d9005e6dce0bb222b4
Divyam04 <divyam04sharma@gmail.com>
2025-01-02 11:31:22 -0500

    fourier series working

--- index.js
+++ index.js
@@ -1,158 +1,387 @@
 // import Two from 'https://cdn.skypack.dev/two.js@latest';
 
+import { TWO_PI } from "two.js/src/utils/math";
 import Two from "./node_modules/two.js/build/two.module.js"
 
-// var two = new Two({
-//   type: Two.Types.svg,
-//   fullscreen: true,
-//   autostart: true
-// }).appendTo(document.body);
 
-// two.renderer.domElement.style.background = '#fcb215';
-// two.renderer.domElement.style.cursor = 'none';
+// import Math
 
-// var cx = two.width / 2;
-// var cy = two.height / 2;
-// var delta = new Two.Vector();
-// var mouse = new Two.Vector(cx, cy);
-// var drag = 0.33;
-// var radius = 50;
+let boundFuncs = [];
 
-// var shape = new Two.Circle(0, 0, radius, 32);
-
-// var shadow = new Two.Path(shape.vertices, true, true);
-// shadow.position.set(cx, cy);
-// shadow.noStroke().fill = 'rgba(0, 0, 0, 0.2)';
-// shadow.offset = new Two.Vector(- radius / 2, radius * 2);
-// shadow.scale = 1.2;
-
-// var ball = new Two.Path(shape.vertices, true, true);
-// ball.position.set(cx, cy);
-// ball.noStroke().fill = 'white';
-
-// for (var i = 0; i < ball.vertices.length; i++) {
-//   var v = ball.vertices[i];
-//   v.origin = v.clone();
-// }
-
-// two.add(shadow, ball);
-
-// window.addEventListener('mousemove', function(e) {
-//   mouse.x = e.clientX;
-//   mouse.y = e.clientY;
-//   shadow.offset.x = 5 * radius * (mouse.x - two.width / 2) / two.width;
-//   shadow.offset.y = 5 * radius * (mouse.y - two.height / 2) / two.height;
-// }, false);
-
-// window.addEventListener('touchstart', function() {
-//   e.preventDefault();
-//   return false;
-// }, false);
-
-// window.addEventListener('touchmove', function(e) {
-//   e.preventDefault();
-//   var touch = e.changedTouches[0];
-//   mouse.x = touch.pageX;
-//   mouse.y = touch.pageY;
-//   shadow.offset.x = 5 * radius * (mouse.x - two.width / 2) / two.width;
-//   shadow.offset.y = 5 * radius * (mouse.y - two.height / 2) / two.height;
-//   return false;
-// }, false);
-
-// two.bind('update', function() {
-
-//   delta.copy(mouse).subSelf(ball.translation);
-  
-//   for (var i = 0; i < ball.vertices.length; i++) {
-
-//     var v = ball.vertices[i];
-//     var dist = v.origin.distanceTo(delta);
-//     var pct = dist / radius;
-
-//     var x = delta.x * pct;
-//     var y = delta.y * pct;
-
-//     var destx = v.origin.x - x;
-//     var desty = v.origin.y - y;
-
-//     v.x += (destx - v.x) * drag;
-//     v.y += (desty - v.y) * drag;
-
-//     shadow.vertices[i].copy(v);
-
-//   }
-
-//   ball.translation.addSelf(delta);
-
-//   shadow.translation.copy(ball.translation);
-//   shadow.translation.addSelf(shadow.offset);
-  
-// });
-
-
-// Result Skip Results Iframe
-// EDIT ON
-
-
-let xPos = 500
-let yPos = 500
-let rad = 50
-
-let pos = [xPos, yPos, rad];
-
-let params = { fullscreen: true};
-
-let elem = document.getElementById("content");
-
-let two = new Two(params).appendTo(elem)
-
-let line = two.makeLine(0,0,rad,0)
-
-// let line = new Two.makeLine(0,0,rad,0)
-
-let circle = two.makeCircle(pos[0], pos[1], pos[2])
-circle.fill =  '#FF8000';
-circle.stroke = 'orangered';
-circle.linewidth = 5;
-circle.opacity = 0.5
-
-let circle2 = two.makeCircle(0 , 0 , 2)
-circle2.fill =  '#aF8F00';
-circle2.stroke = 'blue';
-circle2.linewidth = 2;
-circle2.opacity = 0.5
-
-const group = two.makeGroup(circle2);
-group.translation.set(pos[0],pos[1]);
-
-circle2.position.set(rad,0)
-
-
-line.position.set(pos[0],pos[1]); 
-
-
-
-two.bind("update", update)
-
-// two.add(line)
-two.play();
-
-// line.center([500,500])
-// line.translation.set(500, 500);
-// line.center = [500,500]
-
-function update()
+function drawSignal(two, rotTestX, rotTestY, masterGroup, localRotX,localRotY, lineTest, graphPoints, circleTest, numCycles)
 {
-  // console.log("frameCount: ", frameCount)
-  // rect.rotation+=0.01;
-  // circle.rotation += 0.01;
-  line.rotation += 0.006;
+  rotTestX = masterGroup[0].parent.position.x;
+  rotTestY = masterGroup[0].parent.position.y;
 
-  // circle2.rotation += 0.02
+  let accum = 0;
+  for (let i = 0; i < numCycles ; i++)
+  {
+    let n = (i * 2) + 1 ;
+    let k = n - 2;
 
-  circle2.position.rotate(0.002)
+      let tempTestX = localRotX[i]
+      let tempTestY = localRotY[i]
 
-  group.rotation += 0.005;
+      localRotX[i] =  Math.cos( k * 0.02) * tempTestX - (Math.sin(k * 0.02) * tempTestY);
+      localRotY[i] =  Math.sin( k * 0.02) * tempTestX + (Math.cos(k * 0.02) * tempTestY);
+
+
+    accum = k ; 
+
+    rotTestX = rotTestX + localRotX[i]; 
+    rotTestY = rotTestY + localRotY[i]; 
+
+   }
+   
+
+  lineTest.vertices[0].x = rotTestX; 
+  lineTest.vertices[0].y =  rotTestY; 
+  lineTest.vertices[1].y =  rotTestY; 
+
+  let graphPoint = two.makeCircle(lineTest.vertices[1].x, rotTestY, 1) ;
+
+  // graphPoint.fill =  '#AF2342';
+  graphPoint.stroke = '#AF2342';
+  // graphPoint.linewidth = 0.1;
+  // graphPoint.noStroke(); 
+  graphPoint.opacity = 1;
+
+  graphPoints.push(graphPoint);
+
+  for (let point of graphPoints)
+  {
+    point.translation.set(point.translation.x+1, point.translation.y)
+  }
+
+  if(graphPoints.length > 900)
+  {
+    const removedCircle = graphPoints.shift(); 
+    two.remove(removedCircle); 
+
+  }
+
+  circleTest.translation.set(rotTestX, rotTestY);
+
+  circleTest.fill =  '#000000';
+  circleTest.stroke = 'black';
+  circleTest.linewidth = 1;
+  circleTest.opacity = 0.5;
+
+}
+
+// const drawSignalBound =  drawSignal.bind(this, two, rotTestX, rotTestY, masterGroup, localRotX,localRotY, lineTest, graphPoints, circleTest, numCycles);
+
+function renderHeading(two)
+{
+  const text = two.makeText('Fourier Draw!', window.screen.width/2, 200); // Text, x, y position
+
+text.fill = 'black';       // Set text color
+text.size = 64;            // Set font size
+text.style = "italic"
+text.alignment = 'center'; // Align the text to the center
+return text;
+}
+
+function update(group, rotVal = 0.006)
+{
+  group.rotation += rotVal;
+
+}
+
+function dft(vals)
+{
+  let fourier = [];
+
+  for (let i=0; i<vals.length; i++)
+  {
+    let re = 0;
+    let im = 0; 
+    for(let n=0; n< vals.length; n++)
+    {
+      let angle = (TWO_PI * i * n) / vals.length;
+      re += vals[n] * Math.cos(angle);
+      im -= vals[n] * Math.sin(angle);
+    }
+
+    re = re / vals.length;
+    im = im / vals.length;
+
+    fourier[i] = { re , im };
+  }
+  return fourier
+}
+
+let time = 0;
+function createElement(two, pos, numCycles=5)
+{
+  two.clear();
+
+  renderHeading(two);
+
+
+  // let rad = 50
+
+  // let initFlag = false;
+  let x = 0;
+  let y = 0;
+
+  // let rotCenterX = pos[0];
+  // let rotCenterY = pos[1];
+
+  let translateX = 0;
+  let translateY = 0;
+
+  let currX = 0;
+  let currY = 0;
+
+  const group0 = two.makeGroup();
+  group0.translation.set(pos[0], pos[1])
+
+  // let numCycles = 5;
+  const groupArray = []
+
+  // groupArray.push(group0);
+
+
+  for (let i=0; i < numCycles; i++)
+  {
+    let prevX = x;
+    let prevY = y;
+
+    let n = i * 2 + 1;
+
+    let radius = pos[2] * (4/( n * Math.PI ));
+
+
+    let circle = two.makeCircle(0 , 0, radius)
+    circle.fill =  '#FF8000';
+    circle.stroke = 'orangered';
+    circle.linewidth = 5;
+    circle.opacity = 0.4
+
+
+    let line2 = two.makeLine(0, 0, radius, 0)
+    
+    const group2 = two.makeGroup(circle, line2);
+    group2.translation.set(currX , currY );
+    circle.position.set( translateX,  translateY)
+
+    groupArray.push(group2);
+
+    
+    currX = translateX;
+    currY = translateY;
+
+
+    group0.add(group2);
+
+    x = radius * Math.cos(n * time);
+    y = radius * Math.sin(n * time);
+
+    translateX = x;
+    translateY = y;
+
+    line2.position.set(prevX,  prevY); 
+
+
+    // circle 2 is the ddot in the end
+
+    let circle2 = two.makeCircle(0, 0, 2)
+    circle2.fill =  '#aF8F00';
+    circle2.stroke = 'blue';
+    circle2.linewidth = 2;
+    circle2.opacity = 0.4
+
+
+    circle2.position.set(radius,0)
+    
+
+    // // update for line
+    // two.bind("update", function() {
+    //   update(group); 
+    // } )
+
+  }
+
+  let masterGroup = []
+  for(let i=0 ; i < numCycles ; i++)
+  {
+    let tempGroup = groupArray[i];
+    for (let j = i+1; j<numCycles ; j++)
+    {
+      tempGroup.add(groupArray[j]);
+    }
+    masterGroup.push(tempGroup);
+  }
+
+
+  let rotTestX = 0;
+  let rotTestY = 0;
+  let circleTest = two.makeCircle(rotTestX, rotTestY, 5) ;
+
+  let graphPoints = []
+
+  graphPoints.clear
+
+
+  let localRotX = masterGroup.map(elem => elem.children[0].position.x); 
+  localRotX[groupArray.length - 1] = localRotX[groupArray.length - 1] + masterGroup[groupArray.length - 1].children[0].radius;
+  let localRotY = masterGroup.map(elem => elem.children[0].position.y); 
+
+
+  let lineTest = two.makeLine(rotTestX, rotTestX, 550 +  localRotX.reduce((acc, curr) => acc + curr, 0), 
+      500 + localRotY.reduce((acc, curr) => acc + curr, 0))
+
+  let drawSignalBound =  drawSignal.bind(this, two, rotTestX, rotTestY, masterGroup, localRotX,localRotY, lineTest, graphPoints, circleTest, numCycles);
+  boundFuncs.push(drawSignalBound);
+  two.bind("update", drawSignalBound);
+
+  // two.bind("update", function() 
+  // {
+
+
+  //   rotTestX = masterGroup[0].parent.position.x;
+  //   rotTestY = masterGroup[0].parent.position.y;
+
+  //   let accum = 0;
+  //   for (let i = 0; i < numCycles ; i++)
+  //   {
+  //     let n = (i * 2) + 1 ;
+  //     let k = n - 2;
+
+  //       let tempTestX = localRotX[i]
+  //       let tempTestY = localRotY[i]
+  
+  //       localRotX[i] =  Math.cos( k * 0.02) * tempTestX - (Math.sin(k * 0.02) * tempTestY);
+  //       localRotY[i] =  Math.sin( k * 0.02) * tempTestX + (Math.cos(k * 0.02) * tempTestY);
+
+
+  //     accum = k ; 
+
+  //     rotTestX = rotTestX + localRotX[i]; 
+  //     rotTestY = rotTestY + localRotY[i]; 
+
+  //    }
+     
+
+  //   lineTest.vertices[0].x = rotTestX; 
+  //   lineTest.vertices[0].y =  rotTestY; 
+  //   lineTest.vertices[1].y =  rotTestY; 
+
+  //   let graphPoint = two.makeCircle(lineTest.vertices[1].x, rotTestY, 1) ;
+
+  //   // graphPoint.fill =  '#AF2342';
+  //   graphPoint.stroke = '#AF2342';
+  //   // graphPoint.linewidth = 0.1;
+  //   // graphPoint.noStroke(); 
+  //   graphPoint.opacity = 1;
+
+  //   graphPoints.push(graphPoint);
+
+  //   for (let point of graphPoints)
+  //   {
+  //     point.translation.set(point.translation.x+1, point.translation.y)
+  //   }
+
+  //   if(graphPoints.length > 900)
+  //   {
+  //     const removedCircle = graphPoints.shift(); 
+  //     two.remove(removedCircle); 
+
+  //   }
+
+  //   circleTest.translation.set(rotTestX, rotTestY);
+
+  //   circleTest.fill =  '#000000';
+  //   circleTest.stroke = 'black';
+  //   circleTest.linewidth = 1;
+  //   circleTest.opacity = 0.5;
+
+  // } )
+
+  // // two.bind("update", function() {
+  // //   update(masterGroup[2], 0.02 ); 
+  // // } )
+
+
+    two.bind("update", function() 
+    {
+
+      let accum = 0;
+      for (let i = 1 ; i < numCycles; i++)
+      {
+        let n = (i * 2) + 1 ;
+        let k = n - 2;
+
+      update(masterGroup[i], k * 0.02 ); 
+      update(masterGroup[i], -(accum * 0.02) );
+      accum = k ; 
+     } 
+    } )
+
+
+
+      // two.bind("update", function() {
+      //         // update(masterGroup[2],  0.05 ); 
+      //   update(groupArray[0], 0.05); 
+      // } )
+
 }
 
 
+
+function mainFunc()
+{
+  let params = { fullscreen: true};
+  let elem = document.getElementById("content");
+  let two = new Two(params).appendTo(elem)
+  // elem.style.backgroundColor = '#000000';
+  document.body.style.backgroundColor = "lightblue"
+
+
+  const text = renderHeading(two);
+
+  const slider = document.createElement('input');
+  slider.type = 'range';
+  slider.min = 1;
+  slider.max = 50;
+  slider.value = 10;
+  slider.style.position = 'absolute';
+
+  slider.style.top = text.translation.y+50 + "px";
+  slider.style.left = text.translation.x + "px";
+  document.body.appendChild(slider);
+
+
+
+
+  createElement(two, [500,500,120], slider.value);
+  two.play();
+  // two.unbind();
+  
+  
+  slider.addEventListener( 'input',  function (event) 
+  {
+
+      two.pause();
+      two.unbind("update", boundFuncs[0]);
+      boundFuncs.pop();
+      createElement(two, [500,500,120], slider.value);
+      two.play();
+    
+    },
+    false
+  );
+}
+
+
+// x, y, radius
+// createElement([500,500,120], slider.value);
+
+// let y = [100 , 100 , 100 , -100 , -100, -100,100 , 100 , 100 , -100 , -100, -100];
+
+// console.log(dft(y));
+
+
+
+mainFunc();
